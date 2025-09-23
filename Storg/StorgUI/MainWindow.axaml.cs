9ce337063f40032c33940c93e50ca9589246033b
Lucas SERTELON <lucas.sertelon@gmail.com>
2025-09-23 21:51:48 +0200

    Création du projet Storg

---
+++ Storg/StorgUI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace StorgUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
